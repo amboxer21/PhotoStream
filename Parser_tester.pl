34b76e67e5a2294a29d7bcee5dedb1cef405d350
Amboxer21 <amboxer21@gmail.com>
2015-03-26 22:02:56 -0400

    .

--- Parser_tester.pl
+++ Parser_tester.pl
@@ -7,6 +7,33 @@ use strict;
 my @files = readdir($DIR);
 closedir( $DIR );
 
+my($f);
+foreach my $i (@files) {
+  if($i =~ (/Photostream_Album_[0-9]+/m)) {
+    open $f, '<', $i or die "Cannot open $f: $!\n";
+    while(<$f>) {
+      if(my $next_urls = $_ =~ m/(next\":\"https:\\\/\\\/graph\.facebook\.com\\\/[a-z][0-9]\.[0-9]+\\\/[0-9]+\\\/[a-z]+\?access_token=[a-zA-Z0-9&=]+)/ ) { 
+        $next_urls = $1;
+        $next_urls =~ s/(next\":\")//g;
+	$next_urls =~ s/([\\])//g;
+	#$next_urls =~ s/(:[\"\\]|next)//g;
+        print "NEXT => $next_urls\n";
+      }
+
+      # "source":"https:\/\/scontent.xx.fbcdn.net\/hphotos-xfp1\/v\/t1.0-9\/11025815_817194565026923_2716325669440810711_n.jpg
+      if(my $source_urls = $_ =~ m/(source\":\"https:\\\/\\\/scontent\.[a-zA-Z0-9]+\.fbcdn\.net\\\/[a-zA-Z0-9]+\-[a-zA-Z0-9]+\\\/[a-zA-Z]+\\\/[a-zA-Z0-9-.])/) { 
+        $source_urls = $1;
+        #$source_urls =~ s/(source\":\")//g;
+        #$source_urls =~ s/([\\])//g;
+        #$next_urls =~ s/(:[\"\\]|next)//g;
+        print "SOURCE => $source_urls\n";
+      }
+    }
+    #print "$i\n";
+  }
+}
+
+=cut
    open my $Photostream_photos, '>>', "Photostream_photos" or die "Cannot open Photostream_photos: $!";
    open my $PhotostreamAlbumList, '>>', "PhotostreamAlbumList" or die "Cannot open PhotstreamAlbumList: $!";
    open my $TMP, '>>', "TMP" or die "Cannot open PhotstreamAlbumList: $!";
@@ -14,7 +41,7 @@ closedir( $DIR );
 foreach my $F ( @files ) {
    if( $F =~ m/Photostream_Album/ ) {
    my @regex_array = "$F\n";
-   print "$F\n";
+   #print "$F\n";
    #system qq(cat "$_" >> "$TMP" );
    open my $URLS, $F or die "Cannot open file: $!";
 
@@ -35,3 +62,4 @@ foreach my $F ( @files ) {
 close( $Photostream_photos );
 close( $PhotostreamAlbumList );
 close( $TMP );
+=pod
